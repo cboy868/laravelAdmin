d9f0bcbd6a535913952ce8d891beaed74c6068c1
wansq <cboy868@163.com>
2019-01-13 01:12:03 +0800

    优化路由

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,6 @@ use Illuminate\Http\Request;
 //方法一，通过读取配置获取路由
 Route::group([
     'namespace' => 'api\admin',
-    'middleware' => 'api',
     'prefix' => 'admin'
 ], function ($router) {
     //确定版本
@@ -24,59 +23,20 @@ Route::group([
 
     //确定方法
     $method = \request('method');
-    $action = config('apiadminmap.' . $version . '.' . str_replace('.', '_', $method));
+    $method = str_replace('.', '_', $method);
+    $action = config('apiadminmap.' . $version . '.' . $method);
 
     if (!$action) {
         return $router->post('/', 'V1\ErrorController@noApi');
     }
-    $router->post('/', $version .'\\'. $action);
+
+    //登录无需授权
+    if ($method == 'auth_user_login') {
+        return $router->post('/', $version .'\\'. $action);
+    }
+
+    //其它后台操作均需要授权
+    Route::middleware('auth:api')->group(function ($router)use($version, $action) {
+        $router->post('/', $version .'\\'. $action);
+    });
 });
-
-
-//方法二，通过计算获取路由
-//Route::group([
-//    'namespace' => 'api\admin',
-//    'prefix' => 'admin'
-//], function ($router) {
-//    $adminMap = config('apiadminmap');
-//
-//    //确定方法
-//    $method = \request('method');
-//    $methodStr = "V1\ErrorController@noApi";
-//
-//    //确定版本
-//    $version = \request('version');
-//    $version = 'V' . intval($version);
-//
-//    $method = str_replace_last('.', 'Controller@', $method);
-//    $methodArr = explode('.',$method);
-//
-//    $methodStr = '';
-//    array_walk($methodArr, function ($item, $key) use(&$methodStr) {
-//        $methodStr .= '\\' . ucfirst($item);
-//    });
-//
-//    //如果计算出的路由不存在  怎么办呢
-//
-//    $router->post('/', $version  .$methodStr);
-//});
-
-//Route::group([
-//	'namespace' => 'api\admin\V1',
-//    'prefix' => 'admin'
-//], function ($router) {
-//    Route::post('login', 'AuthController@login');
-//    Route::post('logout', 'AuthController@logout');
-//    Route::post('refresh', 'AuthController@refresh');
-//    Route::post('me', 'AuthController@me');
-//});
-
-
-
-
-//Route::middleware('auth:api')->get('/user', function (Request $request) {
-//    return $request->user();
-//});
-// Route::group(['middleware' => 'auth:api'], function(){
-// 	Route::post('passport', 'Api\Admin\UserController@passport');
-// });
