e7043534d0a2315736508abff2c3e02087d0ef41
wansq <cboy868@163.com>
2019-02-12 16:44:14 +0800

    rbac repository

--- app/Repository/RbacRepository.php
+++ app/Repository/RbacRepository.php
@@ -2,6 +2,10 @@
 
 namespace App\Repository;
 
+use App\User;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
 /**
  *
  * @author: wansq
@@ -12,18 +16,95 @@ namespace App\Repository;
 
 class RbacRepository
 {
-    public function createRole()
+    /**
+     * 创建角色
+     * @param array $attributes
+     * @return $this|\Illuminate\Database\Eloquent\Model
+     */
+    public function createRole(array $attributes=[])
     {
-
+       return Role::create($attributes);
     }
 
-    public function roles()
+    /**
+     * 获取所有角色
+     * @return \Illuminate\Database\Eloquent\Collection|static[]
+     */
+    public function roles(array $where=[])
     {
-
+        return Role::where($where)->get();
     }
+
+    /**
+     * 根据角色名查找角色
+     * @param string $name
+     * @param null $guardName
+     * @return \Spatie\Permission\Contracts\Role|Role
+     */
+    public function findByRoleName(string $name, $guardName = null)
+    {
+        return Role::findByName($name, $guardName);
+    }
+
+    /**
+     * 创建权限项
+     * @param array $attributes
+     * @return $this|\Illuminate\Database\Eloquent\Model
+     */
+    public function createPermission(array $attributes=[])
+    {
+        return Permission::create($attributes);
+    }
+
+    /**
+     * 获取所有权限项
+     * @param array $where
+     * @return mixed
+     */
+    public function permissions(array $where = [])
+    {
+        return Permission::where($where)->get();
+    }
+
+
+    /**
+     * 根据名字获取权限项
+     * @param string $name
+     * @param null $guardName
+     * @return \Spatie\Permission\Contracts\Permission
+     */
+    public function findByPermissionName(string $name, $guardName = null)
+    {
+        return Permission::findByName($name, $guardName);
+    }
+
+
+    /**
+     * todo 待完善
+     * 获取角色下用户
+     * @param string $name
+     * @param string|null $guard_name
+     */
+    public function roleUsers(string $name, string $guard_name=null)
+    {
+        $role = Role::findByName($name, $guard_name);
+    }
+
+    /**
+     * todo 待完善
+     * 获取角色下权限项
+     * @param string $name
+     * @param string|null $guard_name
+     */
+    public function rolePermissions(string $name, string $guard_name=null)
+    {
+        $role = Role::findByName($name, $guard_name);
+    }
+
+
 
     public function userRoles()
     {
-
+        
     }
 }
