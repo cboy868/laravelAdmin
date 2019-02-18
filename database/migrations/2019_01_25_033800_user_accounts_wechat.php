d243fc54911f99d65422ee76a6bb86730323a8ee
wansq <cboy868@163.com>
2019-02-18 16:04:01 +0800

    wechat user account

--- database/migrations/2019_01_25_033800_user_accounts_wechat.php
+++ database/migrations/2019_01_25_033800_user_accounts_wechat.php
@@ -15,8 +15,20 @@ class UserAccountsWechat extends Migration
     {
         Schema::create('user_accounts_wechat', function (Blueprint $table) {
             $table->increments('id');
-
-
+            $table->unsignedInteger('user_id');
+            $table->string('unionid', 255);
+            $table->string('openid', 255);
+            $table->unsignedInteger('user_id');
+            $table->string('nickname');
+            $table->string('remark');
+            $table->tinyInteger('sex');
+            $table->string('language', 100);
+            $table->string('city');
+            $table->string('province');
+            $table->string('country');
+            $table->string('headimgurl');
+            $table->tinyInteger('subscribe');
+            $table->unsignedInteger('subscribe_at');
             $table->timestamps();
         });
     }
@@ -28,6 +40,6 @@ class UserAccountsWechat extends Migration
      */
     public function down()
     {
-        //
+        Schema::drop('user_accounts_wechat');
     }
 }
