5881576758d60b135b245e4db1a2287d5bd66289
wansq <cboy868@163.com>
2019-02-22 15:59:30 +0800

    图片上传

--- app/Http/Requests/UploadFileRequest.php
+++ app/Http/Requests/UploadFileRequest.php
@@ -12,6 +12,16 @@ namespace App\Http\Requests;
 
 class UploadFileRequest extends FormRequest
 {
+
+    /**
+     * 如果 用这个，则传参数时，外面不包params
+     * 否则要修改控制器上传方法
+     * @return array|null|string
+     */
+    protected function validationData()
+    {
+        return $this->input();
+    }
     /**
      * Get the validation rules that apply to the request.
      *
@@ -20,8 +30,8 @@ class UploadFileRequest extends FormRequest
     public function rules()
     {
         return [
-//            'file' => 'image',
-//            'folder' => 'required',
+            'file' => 'image',
+            'folder' => 'required',
         ];
     }
 }
