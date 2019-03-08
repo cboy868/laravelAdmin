306939306fdf7335e4cc1e987ddc5c5820845fbf
wansq <cboy868@163.com>
2019-03-08 15:05:56 +0800

    laravel admin test

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use Encore\Admin\Layout\Content;
+use Encore\Admin\Layout\Row;
 use Illuminate\Http\Request;
 use App\Repository\PostRepository as Post;
 
@@ -12,18 +14,47 @@ class HomeController extends Controller
      *
      * @return void
      */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+//    public function __construct()
+//    {
+//        $this->middleware('auth');
+//    }
 
     /**
      * Show the application dashboard.
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
+    public function index(Content $content)
     {
-        return view('home');
+        $content->header('填写页面头标题');
+
+        // 选填
+        $content->description('填写页面描述小标题');
+
+        // 添加面包屑导航 since v1.5.7
+        $content->breadcrumb(
+            ['text' => '首页', 'url' => '/admin'],
+            ['text' => '用户管理', 'url' => '/admin/users'],
+            ['text' => '编辑用户']
+        );
+
+        // 填充页面body部分，这里可以填入任何可被渲染的对象
+        $content->body('hello world');
+
+
+        $content->row(function(Row $row) {
+            $row->column(4, 'foo');
+            $row->column(4, 'bar');
+            $row->column(4, 'baz');
+        });
+
+        // 在body中添加另一段内容
+        $content->body('foo bar');
+
+        // `row`是`body`方法的别名
+        $content->row('hello world');
+
+        return $content;
+//        return view('home');
     }
 }
