28dba5fedfdc24028f1468b248e7b4adb87d4eea
wansq <cboy868@163.com>
2019-03-22 16:05:12 +0800

    会员级别

---
+++ database/migrations/2019_03_22_080301_alter_user.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterUser extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //添加会员级别
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedTinyInteger('level')->comment("会员级别");
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('level');
+        });
+    }
+}
