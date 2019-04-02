3bfedbda08dfb678e8f360e771163fb8b4dbb02b
wansq <cboy868@163.com>
2019-04-02 17:55:42 +0800

    统一下单

--- app/Http/Controllers/Wechat/Controller.php
+++ app/Http/Controllers/Wechat/Controller.php
@@ -103,11 +103,11 @@ class Controller extends BaseController
             // 必要配置
             'app_id' => 'wxae3fcf7fd4549053',
             'mch_id' => '1518168341',
-            'key' => 'Zhaoyekun23168814711111111111111',   // API 密钥
+            'key' => 'Zhaoyekun23168814722222222222222',   // API 密钥
 
             // 如需使用敏感接口（如退款、发送红包等）需要配置 API 证书路径(登录商户平台下载 API 证书)
-            'cert_path' => 'path/to/your/cert.pem', // XXX: 绝对路径！！！！
-            'key_path' => 'path/to/your/key',      // XXX: 绝对路径！！！！
+//            'cert_path' => 'path/to/your/cert.pem', // XXX: 绝对路径！！！！
+//            'key_path' => 'path/to/your/key',      // XXX: 绝对路径！！！！
 
             'notify_url' => '/api/client/notify',     // 你也可以在下单时单独设置来想覆盖它
         ];
