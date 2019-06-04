15151a10bf8e16e6e9d3bf91fe5041e266150952
wansq <cboy868@163.com>
2019-06-04 10:02:40 +0800

    限制验证码发送次数

--- app/Entities/Sms/Services/Code.php
+++ app/Entities/Sms/Services/Code.php
@@ -18,6 +18,19 @@ class Code
         	//10分钟内有效
         	Cache::put($mobile, $code, 10);
 
+        	//限制一天内10次验证码
+            $key = $mobile . '_times';
+
+            if (Cache::get($key) > 10) {
+                throw new \Exception("获取验证码次数过多");
+            }
+
+            if (Cache::get($key)) {
+                Cache::put($key, Cache::get($key) +1);
+            } else {
+                Cache::put($key, 1);
+            }
+
         	return $code;
         } catch (\Exception $e) {
         	return false;
