e37c11b03858d9089f0f5e6591c6f1fb116c5f10
wansq <cboy868@163.com>
2019-06-07 22:53:32 +0800

    接口控制器添加

---
+++ app/Http/Controllers/Admin/V1/Store/LevelController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers\Admin\V1\Store;
+
+use App\Entities\Store\Repository\LevelRepository;
+use App\Http\Controllers\Admin\AdminController;
+use Illuminate\Http\Request;
+
+class LevelController extends AdminController
+{
+
+    protected $levelRespository;
+
+    public function __construct(LevelRepository $levelRepository)
+    {
+        $this->levelRespository = $levelRepository;
+
+        parent::__construct();
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
